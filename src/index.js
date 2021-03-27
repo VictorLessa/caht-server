2ff85c724230626a7040c89bf6a204b5f45eaa1a
Camara <victor.camara@ksgestao.com.br>
2021-03-27 11:44:37 -0300

    fix: correcao na inicializacao

--- src/index.js
+++ src/index.js
@@ -3,33 +3,8 @@ import Events from "events";
 import Controller from "./controller.js";
 import { constants } from "./constants.js";
 
-async function testeServer() {
-  const opt = {
-    port: 8989,
-    host: "localhost",
-    headers: {
-      Connection: "Upgrade",
-      Upgrade: "webSocket",
-    },
-  };
-  const http = await import("http");
-
-  const server = http.request(opt);
-
-  server.end();
-
-  server.on("upgrade", (req, socket) => {
-    // socket.on("data", (data) => {
-    //   console.log(data.toString());
-    // });
-    // setInterval(() => {
-    //   socket.write("Hello");
-    // }, 500);
-  });
-}
-
 const port = process.env.PORT || 9898;
-
+console.log(port);
 const eventEmitter = new Events();
 const socketServer = new SocketServer(port);
 const server = await socketServer.initialize(eventEmitter);
@@ -41,12 +16,3 @@ eventEmitter.on(
   constants.event.NEW_USER_CONNECTED,
   controller.onNewConnection.bind(controller)
 );
-
-//   console.log(socket.id);
-//   socket.on("data", (data) => {
-//     console.log(data.toString());
-//     socket.write("World");
-//   });
-// });
-
-testeServer();
